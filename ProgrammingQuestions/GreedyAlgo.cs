7cc1303b70e19e31bc12874933a4e466b6496738
Aparna Girme <agirme54@gmail.com>
2024-08-12 12:44:09 -0700

    Solved problems with greedy algo and backtracking algo;

---
+++ ProgrammingQuestions/GreedyAlgo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingQuestions
+{
+    internal class GreedyAlgo
+    {
+        void Main(string[] args)
+        {
+            int[] stockPrice = [10, 7, 5, 8, 11, 9];
+            // Brute Force
+            //int max = 0;
+            //for (int i = 0; i < stockPrice.Length; i++)
+            //{
+            //    for(int j = i + 1; j < stockPrice.Length; j++)
+            //    {
+            //        if (stockPrice[i] < stockPrice[j])
+            //        {
+            //            max = Math.Max(max, stockPrice[j] - stockPrice[i]);
+            //        }
+            //    }
+            //}
+            //Console.WriteLine(max);
+
+            // Greedy Method
+            if (stockPrice.Length < 2)
+            {
+                Console.WriteLine("invalid input");
+                return;
+            }
+
+            int minPrice = stockPrice[0], maxProfit = stockPrice[1] - stockPrice[0];
+            for (int i = 1; i < stockPrice.Length; i++)
+            {
+                var currentPrice = stockPrice[i];
+                maxProfit = Math.Max(maxProfit, currentPrice - minPrice);
+                minPrice = Math.Min(minPrice, currentPrice);
+            }
+            Console.WriteLine(maxProfit);
+            return;
+        }
+    }
+}
---
+++ ProgrammingQuestions/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace ProgrammingQuestions
+{
+    /*
+     Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var digits = "23";
+            var result = LetterCombinations(digits);
+
+            foreach (var str in result)
+            {
+                Console.WriteLine(str);
+            }
+        }
+
+        public static IList<string> LetterCombinations(string digits)
+        {
+            IList<string> result = new List<string>();
+
+            /*
+            Dictionary<char, char[]> keypad = new Dictionary<char, char[]> {{'2', new char[]{'a', 'b', 'c'}}, 
+        {'3', new char[]{'d', 'e', 'f'}}, {'4', new char[] {'g', 'h', 'i'}}, 
+        {'5', new char[] {'j', 'k', 'l'}}, {'6', new char[] {'m', 'n', 'o'}}, 
+        {'7', new char[] {'p', 'q', 'r', 's'}}, {'8', new char[] {'t', 'u', 'v'}}, 
+        {'9', new char[] {'w', 'x', 'y', 'z'}}};
+            */
+            Dictionary<char, string> digitToChar = new Dictionary<char, string>();
+            digitToChar.Add('2', "abc");
+            digitToChar.Add('3', "def");
+            digitToChar.Add('4', "ghi");
+            digitToChar.Add('5', "jkl");
+            digitToChar.Add('6', "mno");
+            digitToChar.Add('7', "pqrs");
+            digitToChar.Add('8', "tuv");
+            digitToChar.Add('9', "wxyz");
+
+            void Backtrack(int i, string curStr)
+            {
+                if (curStr.Length == digits.Length)
+                {
+                    result.Add(curStr);
+                    return;
+                }
+                foreach (var c in digitToChar[digits[i]])
+                {
+                    var newStr = curStr + c;
+                    Backtrack(i + 1, newStr);
+                }
+            }
+            if (digits.Length > 0)
+            {
+                Backtrack(0, "");
+            }
+
+            return result;
+        }
+    }
+}
